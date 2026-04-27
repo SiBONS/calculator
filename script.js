8c0146fea19ade68280c27568e4ab6f43f002033
Mile Kostic <kostic.mile@mail.com>
2026-04-27 19:46:23 +0200

    Programmed the logic for on/off button, reset, backspace and added operators to display

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-const allButtons = document.querySelectorAll(".button");
 const toggleBtn = document.querySelector("#on-off");
 const displayMain = document.getElementById("display-main");
 
+
 const numberButtons = document.querySelectorAll('[data-type="number"]');
 numberButtons.forEach(btn => {
+    btn.addEventListener("click", function() {
+        if(displayMain.textContent === "0") {
+            displayMain.textContent = btn.textContent;
+        } else {
+        displayMain.textContent += btn.textContent;}
+    })
+})
+
+const operators = document.querySelectorAll('[data-type="operator"]');
+operators.forEach(btn => {
     btn.addEventListener("click", function() {
         displayMain.textContent += btn.textContent;
     })
 })
 
+const resetAll = document.getElementById("reset");
+resetAll.addEventListener("click", function() {
+    displayMain.textContent = "0";
+})
 
+
+// Backspace button 
+const backSpace = document.getElementById("backspace");
+backSpace.addEventListener("click", deleteLast => {
+    displayMain.textContent = displayMain.textContent.slice(0, -1) || "0";
+}
+)
+// On/off button
 function onOff() {
     displayMain.textContent = 0;
     if (displayMain.style.display === "none") {
@@ -20,46 +42,18 @@ function onOff() {
     }
 }
 
-
-
-
-
 toggleBtn.addEventListener("click", onOff);
-// allButtons.forEach(button => {
-//     button.addEventListener("click", () => 
-//     {
-//         switch(allButtons) {
-//             case jedan:
-//                 displayMain.innerHTML == "1";
-//             break;
-//             case 2:
-//                 displayMain.textContent = 2;
-//             break;
-//             case 3:
-//                 displayMain.textContent = 3;
-//             break;
-//             case 4:
-//                 displayMain.textContent = 4;
-//             break;
-//             case 5:
-//                 displayMain.textContent = 5;
-//             break;
-//             case 6:
-//                 displayMain.textContent = 6;
-//             break;
-//             case 7:
-//                 displayMain.textContent = 7;
-//             break;
-//             case 8:
-//                 displayMain.textContent = 8;
-//             break;
-//             case 9:
-//                 displayMain.textContent = 9;
-//             break;
-//             case 0:
-//                 displayMain.textContent = 0;
-//             break;
-//             default:
-//         }
-//     })
-// })
+
+// The display of the dot(decimal) 
+const decimal = document.querySelectorAll('[data-type="decimal"]');
+decimal.forEach(dot => {
+    dot.addEventListener("click", function() {
+        if(!displayMain.textContent.includes('.')) {
+            displayMain.textContent += ".";
+        }
+       
+    });
+});
+
+
+
